2673d63ec7481a331d992c7c73e299a598533bc5
coban-54 <92750818+coban-54@users.noreply.github.com>
2022-06-12 19:14:32 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,26 @@
+﻿
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal carDal;
+        //ctor yapıyoruz
+        public CarManager(ICarDal  carDal)
+        {
+            this.carDal = carDal;
+        }
+        public List<Car> GetAll()
+        {
+            //Burda iş yapan kodlar var
+            return carDal.GetAll();
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/CarInMemory.cs
@@ -0,0 +1,62 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class CarInMemory : ICarDal
+    {
+        List<Car> _cars;
+        public CarInMemory()
+        {
+            _cars = new List<Car>
+            {
+                new Car{Id=1,BrandId=1,ColorId=3,DailyPrice=120000,ModelYear="2010",Description="Toyota Corolla sedan otomobil"},
+                new Car{Id=2,BrandId=2,ColorId=1,DailyPrice=300000,ModelYear="2017",Description="Ford Focus Manitanın tercihi"},
+                new Car{Id=3,BrandId=2,ColorId=2,DailyPrice=320000,ModelYear="2019",Description="Ford Fiesta Evlilerin tercihi"},
+                new Car{Id=4,BrandId=3,ColorId=5,DailyPrice=250000,ModelYear="2015",Description="Mitsubishi geniş ve rahat"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll(Car car)
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int Id)
+        {
+            return _cars.Where(c => c.Id == Id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+    }
+}
